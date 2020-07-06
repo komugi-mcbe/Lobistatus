86a25530c4c5a94b4cee9ddb6505dd7a8a9cbf7f
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2020-07-06 22:05:23 +0900

    Add files via upload

---
+++ src/xtakumatutix/lobistatus/Main.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+LobiAPI = https://github.com/NewDelion/LobiAPI-PHP
+参考 =  https://github.com/ikatyo0702/LobiShoutSystem
+*/
+
+namespace xtakumatutix\lobistatus;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use xtakumatutix\lobistatus\LobiAPI;
+
+Class Main extends PluginBase 
+{
+
+    public function onEnable() 
+    {
+        $this->getLogger()->notice("起動メッセージを送信しました - ver.".$this->getDescription()->getVersion());
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, [
+            'mail' => 'test@gmail.com',
+            'password' => '123456',
+            'guild' => 'A1B2C3D4E5F6G7'
+        ]);
+        $message = 'サーバーオープン';
+        $name = '小麦鯖 - Open';
+        $description = '開いています';
+        $this->post($message, $name, $description);
+    }
+
+    public function onDisable()
+    {
+        $message = 'サーバークローズ';
+        $name = '小麦鯖 - Close';
+        $description = '閉じてます';        
+        $this->post($message, $name, $description);
+    }
+
+    public function post($message, $name, $description)
+    {
+        $api = new LobiAPI();
+        $mail = $this->config->get('mail');
+        $password = $this->config->get('password');
+        $guild = $this->config->get('guild');
+        if ($api->Login($mail, $password)){
+            $this->getLogger()->info('メールでのLobiログイン成功');
+            $api->MakeThread($guild, $message, $shout = true);
+            $api->ChangeProfile($name, $description);
+            $this->getLogger()->info('送信完了 - プロフィール変更完了');
+        }else{
+            $this->getLogger()->info('ログイン失敗');
+        }
+    }
+}
